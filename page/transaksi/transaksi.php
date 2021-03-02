ead2729638e102ed33450614c966a36bd249641b
agin123633 <aginbugin11@gmail.com>
2021-03-02 15:34:23 +0700

    merubah file transaksi.php di page transaksi

--- page/transaksi/transaksi.php
+++ page/transaksi/transaksi.php
@@ -1,1 +1,78 @@
-saya halaman transaksi
+<a href="?page=transaksi&aksi=tambah" class="btn btn-success" style="margin-bottom: 5px;"><i class="fa fa-plus"></i> Tambah Data</a>
+
+<div class="row">
+    <div class="col-md-12">
+        <!-- Advanced Tables -->
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                             Data Transaksi
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Judul</th>
+                                            <th>Nim</th>
+                                            <th>Nama</th>
+                                            <th>Tanggal Pinjam</th>
+                                            <th>Tanggal Kembali</th>
+                                            <th>Terlambat</th>
+                                            <th>Status</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $no = 1;
+
+                                        $sql = $koneksi->query("SELECT * FROM tb_transaksi where status='pinjam'");
+
+                                        while ($data= $sql->fetch_assoc()){
+                                        ?>
+
+                                        <tr>
+                                            <td><?php echo $no++; ?></td>
+                                            <td><?php echo $data['judul'] ?></td>
+                                            <td><?php echo $data['nim'] ?></td>
+                                            <td><?php echo $data['nama'] ?></td>
+                                            <td><?php echo $data['tgl_pinjam'] ?></td>
+                                            <td><?php echo $data['tgl_kembali'] ?></td>
+                                            <td>
+                                            
+                                            <?php 
+
+                                            $denda = 1000;
+
+                                            $tgl_dateline2 = $data['tgl_kembali'];
+                                            $tgl_kembali = date('Y-m-d');
+
+                                            $lambat = terlambat($tgl_dateline2, $tgl_kembali);
+                                            $denda1 = $lambat*$denda;
+
+                                            if ($lambat>0) {
+                                                echo "
+                                                <font color='red'>$lambat hari<br>(Rp $denda1)</font>
+                                                ";
+                                            }else {
+                                                echo $lambat ."Hari";
+                                            }
+                                            ?>
+                                            
+                                            </td>
+                                            <td><?php echo $data['status'] ?></td>
+                                            <td>
+                                                <a href="?page=transaksi&aksi=ubah&id=<?php echo $data['nim']; ?>" class="btn btn-info" >Ubah</a>
+                                                <a onclick="return confirm('Apakah anda yakin akan menghapus data ini?')" href="?page=transaksi&aksi=hapus&id=<?php echo $data['nim']; ?>" class="btn btn-danger" >Hapus</a>
+                                            </td>
+                                        </tr>
+
+                                            <?php } ?>
+                                    </tbody>       
+                                
+                            </div>
+                        </div>
+        </div>
+    </div>           
+</div>
