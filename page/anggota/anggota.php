166e873d681a89ed022b3cf0221f6b9f64a6fcc0
agin123633 <aginbugin11@gmail.com>
2021-03-03 15:24:35 +0700

    mengubah file anggota.php di page anggota

--- page/anggota/anggota.php
+++ page/anggota/anggota.php
@@ -1,12 +1,8 @@
-<a href="?page=anggota&aksi=tambah" class="btn btn-success" style="margin-bottom: 5px;"><i class="fa fa-plus"></i> Tambah Data</a>
-
 <div class="row">
     <div class="col-md-12">
         <!-- Advanced Tables -->
         <div class="panel panel-primary">
-            <div class="panel-heading">
-                             Data Anggota
-                        </div>
+            <div class="panel-heading">Data Anggota</div>
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
@@ -36,23 +32,29 @@
 
                                         <tr>
                                             <td><?php echo $no++; ?></td>
-                                            <td><?php echo $data['nim'] ?></td>
-                                            <td><?php echo $data['nama'] ?></td>
-                                            <td><?php echo $data['tempat_lahir'] ?></td>
-                                            <td><?php echo $data['tgl_lahir'] ?></td>
-                                            <td><?php echo $jk ?></td>
-                                            <td><?php echo $prodi ?></td>
+                                            <td><?php echo $data['nim']; ?></td>
+                                            <td><?php echo $data['nama']; ?></td>
+                                            <td><?php echo $data['tempat_lahir']; ?></td>
+                                            <td><?php echo $data['tgl_lahir']; ?></td>
+                                            <td><?php echo $jk; ?></td>
+                                            <td><?php echo $prodi; ?></td>
                                             <td>
-                                                <a href="?page=anggota&aksi=ubah&id=<?php echo $data['nim']; ?>" class="btn btn-info" >Ubah</a>
-                                                <a onclick="return confirm('Apakah anda yakin akan menghapus data ini?')" href="?page=anggota&aksi=hapus&id=<?php echo $data['nim']; ?>" class="btn btn-danger" >Hapus</a>
+                                                <a href="?page=anggota&aksi=ubah&id=<?php echo $data['nim']; ?>" class="btn btn-info" ><i class="fa fa-edit"></i>Ubah</a>
+                                                <a onclick="return confirm('Apakah anda yakin akan menghapus data ini?')" href="?page=anggota&aksi=hapus&id=<?php echo $data['nim']; ?>" class="btn btn-danger" ><i class="fa fa-trash"></i>Hapus</a>
                                             </td>
                                         </tr>
 
                                             <?php } ?>
                                     </tbody>       
-                                
+                                </table>
                             </div>
-                        </div>
+                        <a href="?page=anggota&aksi=tambah" class="btn btn-success" style="margin-top: 8px;"><i class="fa fa-plus"></i> Tambah Data</a>
+                        
+                        <a href="./laporan/laporan_anggota_excel.php" target="blank" class="btn btn-default" style="margin-top: 8px;"><i class="fa fa-print"></i>Export to Excel</a>
+                    </div>
         </div>
-    </div>           
+    </div> 
 </div>
+         
+               
+
