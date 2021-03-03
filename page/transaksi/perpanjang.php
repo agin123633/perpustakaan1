cf19b317394a525aa49408312aa234e1a08a0ddb
agin123633 <aginbugin11@gmail.com>
2021-03-03 15:23:43 +0700

    mengubah file perpanjang.php di page transaksi

--- page/transaksi/perpanjang.php
+++ page/transaksi/perpanjang.php
@@ -14,6 +14,23 @@ if ($lambat > 7) {
     window.location.href="?page=transaksi";
     </script>
     <?php
+
+}else{
+    $pecah_tgl_kembali = explode("-", $tgl_kembali);
+    $next_7_hari = mktime(0,0,0, $pecah_tgl_kembali[1], $pecah_tgl_kembali[0]+7, $pecah_tgl_kembali[2]);
+    $hari_next = date("d-m-Y", $next_7_hari);
+
+    $sql = $koneksi->query("update tb_transaksi set tgl_kembali='$hari_next' where id='$id'");
+
+    if ($sql){
+        ?>
+
+    <script type="text/javascript">
+    alert("PPerpnjangan Berhasi;");
+    window.location.href="?page=transaksi";
+    </script>
+    <?php
+    }
 }
 
 ?>
