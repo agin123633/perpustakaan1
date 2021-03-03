5684a9ef688028d79d59eb0c79d38962559d18fd
agin123633 <aginbugin11@gmail.com>
2021-03-03 13:15:30 +0700

    mengubah file tambah.php di page transaksi

--- page/transaksi/tambah.php
+++ page/transaksi/tambah.php
@@ -69,24 +69,45 @@ $kembali = date("d-m-Y", $tujuh_hari);
 
 <?php
 
-$nim = $_POST ['nim'];
-$nama = $_POST ['nama'];
-$tempat_lahir = $_POST ['tempat_lahir'];
-$tgl_lahir = $_POST ['tgl_lahir'];
-$jk = $_POST ['jk'];
-$prodi = $_POST ['prodi'];
-$simpan = $_POST ['simpan'];
-if ($simpan) {
-    $sql = $koneksi->query("insert into tb_anggota (nim, nama, tempat_lahir, tgl_lahir, jk, prodi) values('$nim','$nama','$tempat_lahir','$tgl_lahir','$jk','$prodi')");
-    
-    if ($sql) {
-        ?>
 
-        <script type="text/javascript">
-        alert ("Data Berhasil Disimpan");
-        window.location.href="?page=anggota";
-        </script>
-        <?php
+
+if (isset($_POST['simpan'])){
+    $tgl_pinjam = $_POST['$tgl_pinjam'];
+    $tgl_kembali = $_POST['$tgl_kembali'];
+
+    $buku = $_POST['buku'];
+    $pecah_buku = explode(".", $buku);
+    $id = $pecah_buku[0];
+    $judul = $pecah_buku[1];
+
+    $nama = $_POST['nama'];
+    $pecah_nama = explode(".", $nama);
+    $nim = $pecah_nama[0];
+    $nama = $pecah_nama[1];
+
+    $sql = $koneksi->query("SELECT * FROM tb_buku where judul = '$judul'");
+    while ($data = $sql->fetch_assoc()) {
+        $sisa = $data['jumlah_buku'];
+
+        if ($sisa == 0) {
+            ?>
+            <script type="text/javascript">
+                alert("Stok Buku Habis, Transaksi Tidak Dapat Dilakukan, Silahkan Tambah Stok Buku Terlebih Dahulu");
+                window.location.href="?page=transaksi&aksi=tambah";
+            </script>
+            <?php
+        }else {
+            $sql = $koneksi->query("insert into tb_transaksi(judul, nim, nama, tgl_pinjam, tgl_kembali, status) values('$judul','$nim','$nama','$tgl_pinjam','$tgl_kembali','pinjam')");
+
+            $sql2 = $koneksi->query("update tb_buku set jumlah_buku=(jumlah_buku-1) where id='$id'");
+
+            ?>
+            <script type="text/javascript">
+                alert("Simpan Data Berhasil");
+                window.location.href="?page=transaksi";
+            </script>
+            <?php
+        }
     }
 }
 
